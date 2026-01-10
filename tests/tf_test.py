231eb69b5357560104e1af5f0d268e5521a85e52
Vitaly <vitaly.neumoin@gmail.com>
2026-01-10 22:09:31 +0300

    откатил ревизию uv, обновил pyproject.toml и доработал ноутбук
    Откатил версию ревизии в файле uv.lock. Обновил pyproject.toml, добавив закомментированные зависимости для CUDA. В ноутбуке скорректировал функции трюков и добавил поле времени выполнения к метаданным ячеек.

---
+++ tests/tf_test.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+
+def main():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            print(e)
+    else:
+        print("No GPU found")
+
+
+if __name__ == '__main__':
+    main()
